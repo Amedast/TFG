3bfd2afd3fb3b14ecd46b212366963caa7f272ef
Amedast <alexmedinaastorga@gmail.com>
2024-05-03 10:43:32 +0200

    Calls Retry fix in case of error

--- client/src/services/media.ts
+++ client/src/services/media.ts
@@ -1,7 +1,24 @@
 import axios, { AxiosResponse } from 'axios'
 import { MediaType, SortType, MediaCardType } from '@/types/media'
+import dotenv from 'dotenv'
+dotenv.config()
+const apiBase = process.env.PUBLIC_API_URL
 
-const apiBase = process.env.NEXT_PUBLIC_API_URL
+import axiosRetry from 'axios-retry'
+
+axiosRetry(axios, {
+  retries: 3,
+  retryDelay: retryCount => {
+    return retryCount * 500
+  },
+  retryCondition: error => {
+    if (!error.response) {
+      return false
+    }
+    const statusCode = error.response.status
+    return statusCode >= 500 && statusCode < 600
+  }
+})
 
 type GetMediaResponse = {
   results: MediaCardType[]
@@ -19,17 +36,27 @@ export const apiGetMediaList = async (
   type: MediaType,
   sortType: SortType
 ): Promise<AxiosResponse<GetMediaResponse>> => {
-  const res = await axios.get<GetMediaResponse>(
-    `${apiBase}/api/medialist?type=${type}&sorttype=${sortType}`
-  )
-  return res
+  try {
+    const res = await axios.get<GetMediaResponse>(
+      `${apiBase}/api/medialist?type=${type}&sorttype=${sortType}`
+    )
+    return res
+  } catch (error) {
+    console.error('Error al obtener la lista de medios:', error)
+    throw error
+  }
 }
 
 export const apiGetTrendingMediaList = async (): Promise<
   AxiosResponse<GetTrendingResponse>
 > => {
-  const res = await axios.get<GetTrendingResponse>(
-    `${apiBase}/api/trendinglist`
-  )
-  return res
+  try {
+    const res = await axios.get<GetTrendingResponse>(
+      `${apiBase}/api/trendinglist`
+    )
+    return res
+  } catch (error) {
+    console.error('Error al obtener la lista de medios en tendencia:', error)
+    throw error
+  }
 }
